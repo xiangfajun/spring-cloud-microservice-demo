c30d6aac89adbacff4f1a513d6784f6f2ba1a52f
fajun.xiang <xfj@sxw.cn>
2018-08-02 13:57:53 +0800

    提交hystrix中增加的一个配置，只有在开启之后，才会打开断路器。

--- consumer-feign-with-hystrix-demo/src/main/java/com/xiangfajun/cloud/demo/user/feign/UserFeignHystrixClient.java
+++ consumer-feign-with-hystrix-demo/src/main/java/com/xiangfajun/cloud/demo/user/feign/UserFeignHystrixClient.java
@@ -5,7 +5,6 @@ import com.xiangfajun.cloud.demo.user.fallback.UserFeignHystrixClientFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 使用@FeignClient注解的fallback属性，指定fallback类
@@ -14,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface UserFeignHystrixClient {
 
   @RequestMapping("/{id}")
- public User findByIdFeign(@PathVariable("id") Long id);
+  User findByIdFeign(@PathVariable("id") Long id);
 
   @RequestMapping("/time_out/{time}")
-  public User testTimeOutFeign(@PathVariable("time") Long time);
+  User testTimeOutFeign(@PathVariable("time") Long time);
 
 }
